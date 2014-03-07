8cb55f81f55ad3627ee4d342343fc2860d2404dc
Alex Konrad <alexkonrad08@gmail.com>
2014-03-07 18:03:13 -0500

    fix notification hide

--- app/assets/javascripts/views/posts/posts_footer.js
+++ app/assets/javascripts/views/posts/posts_footer.js
@@ -5,19 +5,17 @@ Facebook.Views.PostFooter = Backbone.View.extend({
     this.listenTo(this.collection, "add remove", this.render);
 	},
 	render: function () {
-		var likeView = new Facebook.Views.LikesShow({
-			model: this.model,
-      collection: this.collection
-		});
-		this.$el.html(likeView.render().$el);
+	  var likeView = new Facebook.Views.LikesShow({
+	    model: this.model,
+        collection: this.collection
+      });
+	  this.$el.html(likeView.render().$el);
 
     var renderedPostFooter = JST['posts/post_footer']({
       post: this.model
     });
 
     this.$el.append(renderedPostFooter);
-
-
 
 		return this;
 	}
