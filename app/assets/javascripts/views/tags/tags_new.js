d31eae8aa084d0c7bc1ff1a0fa312992d64c03fb
Alex Konrad <alexkonrad08@gmail.com>
2014-03-07 21:10:10 -0500

    add tagging form

--- app/assets/javascripts/views/tags/tags_new.js
+++ app/assets/javascripts/views/tags/tags_new.js
@@ -2,12 +2,20 @@ Facebook.Views.TagsNew = Backbone.View.extend({
   initialize: function () {},
   className: "",
   template: JST["tags/new"],
-  events: "",
+  events: {
+    "submit" : "submit"
+  },
   render: function () {
     var renderedNewTag = this.template({});
     
     this.$el.html(renderedNewTag);
     
     return this;
+  },
+  submit: function (event) {
+    event.preventDefault();
+    
+    var data = this.$el.serializeJSON();
+    console.log(data);
   }
 })
